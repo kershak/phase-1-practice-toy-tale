9702270679a6020264ea4028fa19163c316c5907
Nills <yakusaz17@gmail.com>
2024-08-22 07:43:04 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-let addToy = false;
-
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-});
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let addToy = false
+let divCollect = document.querySelector('#toy-collection')
 
 function getToys() {
   return fetch('http://localhost:3000/toys')
